02ddb83617e77b59d9c535c3f92b4def8c89908b
Ruan Likun <85725319+MoeField@users.noreply.github.com>
2024-12-04 23:44:07 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -17,8 +17,8 @@ echo $filename
 [ -f $filename ] && { echo "File exist, end progress"; exit 0; }
 wget  --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" --header="Accept-Encoding: compress, gzip"  $url -O $filename
 echo "Downloaded $filename"
-#unzip the server to the server folder overwrite the files
-[ -d "../server" ] || { mkdir ../server; }
-unzip -o $filename -d ../server
+##unzip the server to the server folder overwrite the files
+#[ -d "../server" ] || { mkdir ../server; }
+unzip -o $filename -d ..
 echo "Unzipped $filename to server folder"
 cd ..
