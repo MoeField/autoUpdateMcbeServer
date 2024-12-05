8929bfd493d154fbf3943e70e378e60865c7faae
Ruan Likun <85725319+MoeField@users.noreply.github.com>
2024-12-05 18:39:46 +0800

    Update update.sh
    stable shell script(?

--- update.sh
+++ update.sh
@@ -1,24 +1,77 @@
 #!/bin/bash
+
+# Function to find the download URLs of the latest version of Minecraft Bedrock server
+function findurl(){
+  url="https://www.minecraft.net/en-us/download/server/bedrock"
+  headers=(
+    "-H" "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    "-H" "Connection: keep-alive"
+    "-H" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+    "-H" "Accept-Language: en-us,zh-CN,zh;q=0.8"
+  )
+
+  html=$(curl -s "${headers[@]}" "$url")
+
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to fetch the page"
+    exit 1
+  fi
+
+  urls=$(echo "$html" | grep -oP 'https://www.minecraft.net/bedrockdedicatedserver/bin-[^"]+')
+
+  declare -A dict
+  while IFS= read -r url; do
+    if [[ $url == *"bin-win-preview/"* ]]; then
+      dict["win-preview"]=$url
+    elif [[ $url == *"bin-linux-preview/"* ]]; then
+      dict["linux-preview"]=$url
+    elif [[ $url == *"bin-win/"* ]]; then
+      dict["win"]=$url
+    elif [[ $url == *"bin-linux/"* ]]; then
+      dict["linux"]=$url
+    fi
+  done <<< "$urls"
+
+  #echo $sys
+  echo ${dict[$sys]}
+}
+
+#Default Settings
+dl_dir="srv-core-download"
+preview=false
+sys="linux"
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -p) preview=true ;;
+    --sys) sys="$2"; shift ;;
+    *) echo "Unknown parameter passed: $1"; exit 1 ;;
+  esac
+  shift
+done
+serverVersions=("win" "linux")
+if [[ ! " ${serverVersions[@]} " =~ " ${sys} " ]]; then
+  echo "--sys only accept 'win'/'linux' !"
+  exit 1
+fi
+if $preview; then
+  sys+="-preview"
+fi
+
 echo "Start update progress"
-#cd to the script folder
-pwd | grep -q "autoUpdateMcbeServer" || { cd autoUpdateMcbeServer; }
-#git pull
-cd ./bin
-source activate
-cd ../..
 #makedir download if not exist then download the server
-[ -d "srv-core-download" ] || { mkdir srv-core-download; }
-cd srv-core-download
+[ -d "$dl_dir" ] || { mkdir $dl_dir; }
+cd $dl_dir
 #get the download url
-url=$(python3 ../autoUpdateMcbeServer/findDlUrls.py)
+url=$(findurl $sys)
 filename=$(basename $url)
-echo $filename
+echo "latest server file: $filename"
 #if file exist then end the script
-[ -f $filename ] && { echo "File exist, end progress"; exit 0; }
-wget  --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" --header="Accept-Encoding: compress, gzip"  $url -O $filename
-echo "Downloaded $filename"
+[ -f $filename ] && { echo "Server File already exist, no need to update!"; exit 0; }
+curl -H "User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" -H "Accept-Encoding: compress, gzip" -o "$filename" "$url"
+echo "Downloaded $filename, unzipping"
 ##unzip the server to the server folder overwrite the files
 #[ -d "../server" ] || { mkdir ../server; }
-unzip -o $filename -d ..
-echo "Unzipped $filename to server folder"
+unzip -qo $filename -d ../
+echo "Unzipped $filename"
 cd ..
