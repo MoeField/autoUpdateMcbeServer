0b0366ca88c97e5773508bcd870d3b0b7077e412
Ruan Likun <85725319+MoeField@users.noreply.github.com>
2024-12-05 17:42:50 +0800

    Update findDlUrls.sh
    use ` -p ` for preview, use ` --sys [win|linux] ` for system

--- findDlUrls.sh
+++ findDlUrls.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 
 # Function to find the download URLs of the latest version of Minecraft Bedrock server
-findMcBeServerUrls() {
-  url="https://www.minecraft.net/en-us/download/server/bedrock"
-  headers=(
-    "-H" "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
-    "-H" "Connection: keep-alive"
-    "-H" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-    "-H" "Accept-Language: en-us,zh-CN,zh;q=0.8"
-  )
 
-  html=$(curl -s "${headers[@]}" "$url")
+url="https://www.minecraft.net/en-us/download/server/bedrock"
+headers=(
+  "-H" "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+  "-H" "Connection: keep-alive"
+  "-H" "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+  "-H" "Accept-Language: en-us,zh-CN,zh;q=0.8"
+)
 
-  if [ $? -ne 0 ]; then
-    echo "Error: Failed to fetch the page"
-    exit 1
+html=$(curl -s "${headers[@]}" "$url")
+
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to fetch the page"
+  exit 1
+fi
+
+urls=$(echo "$html" | grep -oP 'https://www.minecraft.net/bedrockdedicatedserver/bin-[^"]+')
+
+declare -A dict
+while IFS= read -r url; do
+  if [[ $url == *"bin-win-preview/"* ]]; then
+    dict["win-preview"]=$url
+  elif [[ $url == *"bin-linux-preview/"* ]]; then
+    dict["linux-preview"]=$url
+  elif [[ $url == *"bin-win/"* ]]; then
+    dict["win"]=$url
+  elif [[ $url == *"bin-linux/"* ]]; then
+    dict["linux"]=$url
   fi
+done <<< "$urls"
 
-  urls=$(echo "$html" | grep -oP 'https://www.minecraft.net/bedrockdedicatedserver/bin-[^"]+')
-
-  declare -A dict
-  while IFS= read -r url; do
-    if [[ $url == *"bin-win-preview"* ]]; then
-      dict["win-preview"]=$url
-    elif [[ $url == *"bin-linux-preview"* ]]; then
-      dict["linux-preview"]=$url
-    elif [[ $url == *"bin-win"* ]]; then
-      dict["win"]=$url
-    elif [[ $url == *"bin-linux"* ]]; then
-      dict["linux"]=$url
-    fi
-  done <<< "$urls"
-
-  echo "${dict[@]}"
-}
-
-# Main script
 preview=false
 sys="linux"
 
@@ -58,5 +54,5 @@ if $preview; then
   sys+="-preview"
 fi
 
-urls=$(findMcBeServerUrls)
-echo "$urls" | grep -oP "(?<=${sys}=)[^ ]+"
+#echo $sys
+echo ${dict[$sys]}
